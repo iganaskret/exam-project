b887076fb99f3d2510d3f06df2a70935d6ead4d0
Iga <iga.naskret@gmail.com>
2019-06-03 19:47:01 +0200

    new pic

--- js.js
+++ js.js
@@ -4,42 +4,14 @@ const barLogo = document.querySelector(".bar-logo");
 const modalSizes = document.querySelector(".modal-sizes-bckg");
 const size = document.querySelector("#size");
 
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    };
-
-    slides[slideIndex - 1].style.display = "block";
-};
-
-modalSizes.addEventListener("click", () => modalSizes.classList.add("hide"));
-size.addEventListener("click", () => modalSizes.classList.remove("hide"));
-
 hamburger.addEventListener("click", () => {
     burgerMenu.classList.toggle("hide");
     burgerMenu.style.zIndex = "1";
 
     barLogo.classList.toggle("hide");
 });
+
+
+modalSizes.addEventListener("click", () => modalSizes.classList.add("hide"));
+size.addEventListener("click", () => modalSizes.classList.remove("hide"));
+
