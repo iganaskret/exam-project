7706adfb372fcd385c0761ea8a695f81d878c350
Andrea Makarová <ajka@MacBook-Pro-uzivatela-Andrea.local>
2019-06-03 22:54:47 +0200

    prints

--- js.js
+++ js.js
@@ -3,17 +3,9 @@ const hamburger = document.querySelector(".hamburger");
 const barLogo = document.querySelector(".bar-logo");
 const modalSizes = document.querySelector(".modal-sizes-bckg");
 const size = document.querySelector("#size");
-const details = document.querySelector("#button1");
-const modal = document.querySelector("#modal");
 
-<<<<<<< HEAD
 
-details.addEventListener("click", () => modal.classList.remove("hide"));
-modalSizes.addEventListener("click", () => modalSizes.classList.add("hide"));
-size.addEventListener("click", () => modalSizes.classList.remove("hide"));
 
-=======
->>>>>>> origin/master
 hamburger.addEventListener("click", () => {
     burgerMenu.classList.toggle("hide");
     burgerMenu.style.zIndex = "1";
