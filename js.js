243b8911accc8bc76f88daf05363fb889738cae1
Iga <iga.naskret@gmail.com>
2019-06-03 23:07:08 +0200

    js

--- js.js
+++ js.js
@@ -4,16 +4,17 @@ const barLogo = document.querySelector(".bar-logo");
 const modalSizes = document.querySelector(".modal-sizes-bckg");
 const size = document.querySelector("#size");
 
-details.addEventListener("click", () => modal.classList.remove("hide"));
-modalSizes.addEventListener("click", () => modalSizes.classList.add("hide"));
-size.addEventListener("click", () => modalSizes.classList.remove("hide"));
-
 hamburger.addEventListener("click", () => {
     burgerMenu.classList.toggle("hide");
     burgerMenu.style.zIndex = "1";
 
     barLogo.classList.toggle("hide");
 });
+
+details.addEventListener("click", () => modal.classList.remove("hide"));
+modalSizes.addEventListener("click", () => modalSizes.classList.add("hide"));
+size.addEventListener("click", () => modalSizes.classList.remove("hide"));
+
 
 
 
