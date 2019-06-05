99b3a290b39d0c8f4e0ac166a4b8722c1f9e1c44
Iga <iga.naskret@gmail.com>
2019-06-05 20:27:45 +0200

    e-mail

--- handler.php
+++ handler.php
@@ -21,6 +21,6 @@ $validator->field('comments')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('iga.naskret@gmail.com'); // ← Your email here
+$pp->sendEmailTo('andry.deliana@gmail.com'); // ← Your email here
 
 echo $pp->process($_POST);
